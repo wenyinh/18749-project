ca3ed650f04ac03135ebd69546272c0fafc946e1
wenyinh <wenyinh@andrew.cmu.edu>
2025-09-18 16:53:49 -0400

    Add utils

--- utils/utils.go
+++ utils/utils.go
@@ -1,1 +1,37 @@
 package utils
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"strings"
+)
+
+func WriteLine(conn net.Conn, s string) error {
+	_, err := conn.Write([]byte(s + "\n"))
+	return err
+}
+
+func ReadLine(r *bufio.Reader) (string, error) {
+	line, err := r.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimRight(line, "\r\n"), nil
+}
+
+func MustListen(addr string) net.Listener {
+	ln, err := net.Listen("tcp", addr)
+	if err != nil {
+		panic(fmt.Errorf("listen %s failed: %w", addr, err))
+	}
+	return ln
+}
+
+func MustDial(addr string) net.Conn {
+	c, err := net.Dial("tcp", addr)
+	if err != nil {
+		panic(fmt.Errorf("dial %s failed: %w", addr, err))
+	}
+	return c
+}
