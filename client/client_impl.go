c6ede2b3deb120dcd890a743420b25c805a3653f
wenyinh <wenyinh@andrew.cmu.edu>
2025-09-21 01:54:54 -0400

    fmt->log

--- client/client_impl.go
+++ client/client_impl.go
@@ -1,8 +1,8 @@
 package client
 
 import (
-	"fmt"
 	"github.com/wenyinh/18749-project/utils"
+	"log"
 	"net"
 )
 
@@ -22,19 +22,19 @@ func NewClient(clientID int, serverAddr string) Client {
 func (c *client) Connect() error {
 	conn := utils.MustDial(c.serverAddr)
 	c.conn = conn
-	fmt.Printf("[C%d] Connected to server S1\n", c.clientID)
+	log.Printf("[C%d] Connected to server S1\n", c.clientID)
 	return nil
 }
 
 func (c *client) SendMessage(message string) {
 	if c.conn == nil {
-		fmt.Printf("[C%d] Not connected to server\n", c.clientID)
+		log.Printf("[C%d] Not connected to server\n", c.clientID)
 		return
 	}
-	fmt.Printf("[C%d] Sending request: %s\n", c.clientID, message)
+	log.Printf("[C%d] Sending request: %s\n", c.clientID, message)
 	err := utils.WriteLine(c.conn, message)
 	if err != nil {
-		fmt.Printf("[C%d] Error sending message: %v\n", c.clientID, err)
+		log.Printf("[C%d] Error sending message: %v\n", c.clientID, err)
 		return
 	}
 
@@ -42,16 +42,16 @@ func (c *client) SendMessage(message string) {
 	buffer := make([]byte, 1024)
 	n, err := c.conn.Read(buffer)
 	if err != nil {
-		fmt.Printf("[C%d] Error receiving reply: %v\n", c.clientID, err)
+		log.Printf("[C%d] Error receiving reply: %v\n", c.clientID, err)
 		return
 	}
 	reply := string(buffer[:n])
-	fmt.Printf("[C%d] Received reply: %s\n", c.clientID, reply)
+	log.Printf("[C%d] Received reply: %s\n", c.clientID, reply)
 }
 
 func (c *client) Close() {
 	if c.conn != nil {
 		c.conn.Close()
-		fmt.Printf("[C%d] Disconnected from server S1\n", c.clientID)
+		log.Printf("[C%d] Disconnected from server S1\n", c.clientID)
 	}
 }
