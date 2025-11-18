186db6de1d847f801e59ede58a60e3a2f6e68190
wenyinh <wenyinh@andrew.cmu.edu>
2025-11-17 19:04:21 -0500

    add start cmd comment

--- cmd/gfd/grunner.go
+++ cmd/gfd/grunner.go
@@ -8,11 +8,14 @@ import (
 	"github.com/wenyinh/18749-project/gfd"
 )
 
+// go run cmd/gfd/grunner.go \
+// -addr 0.0.0.0:8000 \
+// -rm_addr 127.0.0.1:8001
 func main() {
 	addr := flag.String("addr", "0.0.0.0:8000", "GFD listen address")
 	hb := flag.Duration("hb", 1*time.Second, "heartbeat frequency")
 	timeout := flag.Duration("timeout", 3*time.Second, "heartbeat timeout")
-	rmAddr := flag.String("rm_addr", "127.0.0.1:7000", "RM address")
+	rmAddr := flag.String("rm_addr", "127.0.0.1:8001", "RM address")
 	flag.Parse()
 
 	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
--- cmd/rm/rrunner.go
+++ cmd/rm/rrunner.go
@@ -7,8 +7,9 @@ import (
 	"github.com/wenyinh/18749-project/rm"
 )
 
+// go run cmd/rm/rrunner.go -addr 0.0.0.0:8001
 func main() {
-	addr := flag.String("addr", "0.0.0.0:7000", "RM listen address")
+	addr := flag.String("addr", "0.0.0.0:8001", "RM listen address")
 	flag.Parse()
 
 	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
